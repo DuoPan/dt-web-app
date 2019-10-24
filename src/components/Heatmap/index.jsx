f4d925c54342169267805358ecc35981cf2fcaf8
DuoPan <pd54007@gmail.com>
2019-10-24 22:06:39 +1100

    New package of heatmap.

--- src/components/Heatmap/index.jsx
+++ src/components/Heatmap/index.jsx
@@ -3,64 +3,91 @@ import { connect } from 'react-redux'
 import echarts from 'echarts/lib/echarts';
 import 'echarts/lib/chart/heatmap';
 import * as styles from '../../containers/DemoHomePage/styles.scss'
+import HeatmapGrid from 'react-heatmap-grid';
 
 function Heatmap({heatmapOption, serialNumber}) {
-  const chartRef = useRef(null);
-  let chartInstance = null;
+  // const chartRef = useRef(null);
+  // let chartInstance = null;
+  //
+  // useEffect(
+  //   () => {
+  //     const renderedInstance = echarts.getInstanceByDom(chartRef.current)
+  //     if (renderedInstance) {
+  //       chartInstance = renderedInstance
+  //     } else {
+  //       chartInstance = echarts.init(chartRef.current)
+  //     }
+  //
+  //     const options = {
+  //       grid: {
+  //         height: '50%',
+  //         y: '10%'
+  //       },
+  //       xAxis: {
+  //         show: false,
+  //       },
+  //       yAxis: {
+  //         show: false,
+  //       },
+  //       visualMap: {
+  //         min: 0,
+  //         max: 20,
+  //         calculable: true,
+  //         orient: 'horizontal',
+  //         left: 'center',
+  //         bottom: '15%',
+  //         color: ['#d94e5d','#eac736','#50a3ba'],
+  //       },
+  //       series: [{
+  //         name: 'Punch Card',
+  //         type: 'heatmap',
+  //         data: heatmapOption.data[serialNumber.cur],
+  //         label: {
+  //           normal: {
+  //             show: true
+  //           }
+  //         },
+  //         itemStyle: {
+  //           emphasis: {
+  //             shadowBlur: 10,
+  //             shadowColor: 'rgba(0, 0, 0, 0.5)'
+  //           }
+  //         }
+  //       }]
+  //     };
+  //     chartInstance.setOption(options);
+  //   },
+  //   [serialNumber.cur]
+  // );
+  //
+  // return (
+  //   <div ref={chartRef} className={styles.chart}/>
+  // );
 
-  useEffect(
-    () => {
-      const renderedInstance = echarts.getInstanceByDom(chartRef.current)
-      if (renderedInstance) {
-        chartInstance = renderedInstance
-      } else {
-        chartInstance = echarts.init(chartRef.current)
-      }
 
-      const options = {
-        grid: {
-          height: '50%',
-          y: '10%'
-        },
-        xAxis: {
-          show: false,
-        },
-        yAxis: {
-          show: false,
-        },
-        visualMap: {
-          min: 0,
-          max: 20,
-          calculable: true,
-          orient: 'horizontal',
-          left: 'center',
-          bottom: '15%',
-          color: ['#d94e5d','#eac736','#50a3ba'],
-        },
-        series: [{
-          name: 'Punch Card',
-          type: 'heatmap',
-          data: heatmapOption.data[serialNumber.cur],
-          label: {
-            normal: {
-              show: true
-            }
-          },
-          itemStyle: {
-            emphasis: {
-              shadowBlur: 10,
-              shadowColor: 'rgba(0, 0, 0, 0.5)'
-            }
-          }
-        }]
-      };
-      chartInstance.setOption(options);
-    },
-    [serialNumber.cur]
-  );
+  if (serialNumber.cur < 0 || serialNumber.cur >= heatmapOption.data.length) {
+    return (<div/>);
+  }
+
+  const xLabels = new Array(heatmapOption.data[serialNumber.cur][0].length).fill(0).map((_, i) => '');
+  const yLabels = new Array(heatmapOption.data[serialNumber.cur].length).fill(0).map((_, i) => '');
+  const data=heatmapOption.data[serialNumber.cur];
 
   return (
-    <div ref={chartRef} className={styles.chart}/>
+    <HeatmapGrid
+      xLabels={xLabels}
+      yLabels={yLabels}
+      data={data}
+      background={'green'}
+      height={20}
+      squares={true}
+      unit={'pd'}
+      // cellStyle={(background, value, min, max, data, x, y) => ({
+      //   background: `rgba(66, 86, 244, ${1 - (max - value) / (max - min)})`,
+      //   fontSize: "11px",
+      // })}
+      // cellRender={value => value && `${value}`}
+    />
   );
 }
 
--- src/reducers/heatmapOption.js
+++ src/reducers/heatmapOption.js
@@ -6,15 +6,60 @@ const initialState = {
   data: [],
 };
 
+// export default function (state = initialState, action) {
+//   switch (action.type) {
+//     case actionTypes.LOAD_HEAT_MAP_OPTION_SUCCESS: {
+//       const { data } = action.payload;
+//       let _data = [];
+//       data.forEach(function (elem) {
+//         _data.push(elem.heatmap.map(function (item) {
+//           return [item[1], item[0], item[2] || '-'];
+//         }))
+//       });
+//       return {
+//         ...state,
+//         status: dataStatus.SUCCESS,
+//         data: _data,
+//       };
+//     }
+//     case actionTypes.RESET_ALL_IMAGES: {
+//       return initialState;
+//     }
+//     default:
+//       return state;
+//   }
+// }
+
 export default function (state = initialState, action) {
   switch (action.type) {
     case actionTypes.LOAD_HEAT_MAP_OPTION_SUCCESS: {
       const { data } = action.payload;
       let _data = [];
+      let _single = [];
+      let _x = 0;
+      let _y = 0;
       data.forEach(function (elem) {
-        _data.push(elem.heatmap.map(function (item) {
-          return [item[1], item[0], item[2] || '-'];
-        }))
+        let hm = elem.heatmap;
+        _single = [];
+        hm.forEach(function(item) {
+          if(item[1] > _x) {
+            _x = item[1];
+          }
+          if(item[0] > _y) {
+            _y = item[0];
+          }
+        });
+       // console.log(_x, _y,'------');//23, 6
+
+        for (let j = 0 ;j <= _y; ++ j) {
+          let _a = [];
+          for (let i = 0 ; i <= _x; ++ i) {
+            _a.push(hm[i+(_x+1)*j][2] + 1);
+          }
+          _single.push(_a);
+        }
+        let _newSingle = _single.reverse();
+        _data.push(_newSingle);
       });
       return {
         ...state,
